871811421c2eb37a2655996c26fd95ccf454811e
윤미(Youn Mee) <cocomong_94@naver.com>
2021-09-24 19:44:03 +0900

    20210924_up

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-alert("Hi!");
+
+
+function resized() {
+  if (window.innerWidth > 1000) {
+    document.body.style.backgroundColor = "orange";
+  } else if (window.innerWidth > 500 && window.innerWidth <= 900) {
+    document.body.style.backgroundColor = "yellowgreen";
+  } else {
+    document.body.style.backgroundColor = "skyblue";
+  }
+}
+
+window.addEventListener("resize", resized);
